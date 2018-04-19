3393af5f9266e69c79d2ba68384a3815cf9796e2
benschulman <bgschulman31@gmail.com>
2018-04-19 15:25:03 -0500

    made Bracket and Team classes

---
+++ src/Bracket.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Bracket {
+	private ArrayList<Team> competition;
+	private Team champion;
+	private Team runnerUp;
+	private Team third;
+
+	public Bracket(){
+		this.competition = new ArrayList<>();
+	}
+}
---
+++ src/Team.java
@@ -0,0 +1,9 @@
+public class Team {
+	private String teamName;
+	private Integer teamRank;
+
+	public Team(String teamName, Integer teamRank){
+		this.teamName = teamName;
+		this.teamRank = teamRank;
+	}
+}
