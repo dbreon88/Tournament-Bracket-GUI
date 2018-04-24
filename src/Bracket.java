32b30cbe3044e9523f8642ac5230ad719d226d14
steven-wood21 <37051521+steven-wood21@users.noreply.github.com>
2018-04-23 21:25:48 -0500

    Added comments

--- src/Bracket.java
+++ src/Bracket.java
@@ -1,11 +1,29 @@
 import java.util.ArrayList;
 
+////////////////////////////////////////////////////////////////////////////
+//Semester:         CS400 Spring 2018
+//PROJECT:          Tournament Bracket GUI
+//
+//Authors:          Steven Wood, Jacob Latts, Ben Schulman, Dylan Breon
+//
+//Instructor:       Deb Deppeler (deppeler@cs.wisc.edu)
+//Bugs:             No known bugs
+//
+//2018 Apr 23, 2018 Bracket.java
+////////////////////////////////////////////////////////////////////////////
+
+/**
+ * This class represents a bracket with the teams in competition, and the top three placements.
+ */
 public class Bracket {
 	private ArrayList<Team> competition;
 	private Team champion;
 	private Team runnerUp;
 	private Team third;
-
+	
+	/**
+	 * Constructor for bracket that initializes the competition list.
+	 */
 	public Bracket(){
 		this.competition = new ArrayList<>();
 	}
