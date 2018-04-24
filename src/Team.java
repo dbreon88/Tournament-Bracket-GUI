304c63f0d2af3dd2f499807986125716009c01e0
steven-wood21 <37051521+steven-wood21@users.noreply.github.com>
2018-04-23 21:29:03 -0500

    Added comments

--- src/Team.java
+++ src/Team.java
@@ -1,7 +1,28 @@
+////////////////////////////////////////////////////////////////////////////
+//Semester:         CS400 Spring 2018
+//PROJECT:          Tournament Bracket GUI
+//
+//Authors:          Steven Wood, Jacob Latts, Ben Schulman, Dylan Breon
+//
+//Instructor:       Deb Deppeler (deppeler@cs.wisc.edu)
+//Bugs:             No known bugs
+//
+//2018 Apr 23, 2018 Team.java
+////////////////////////////////////////////////////////////////////////////
+
+/**
+ * This class represents a team that has a team name's and it's rank.
+ */
 public class Team {
 	private String teamName;
 	private Integer teamRank;
-
+	
+	/**
+	 * Constructor that creates a team with a given name and starting rank
+	 * 
+	 * @param teamName Team's name
+	 * @param teamRank Starting rank
+	 */
 	public Team(String teamName, Integer teamRank){
 		this.teamName = teamName;
 		this.teamRank = teamRank;
