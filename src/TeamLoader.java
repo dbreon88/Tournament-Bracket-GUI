93c88fd4e844e6c59cce191fadb15e916e818fdb
Steven Wood <sdwood3@wisc.edu>
2018-04-29 13:46:47 -0500

    Added loading of teams

---
+++ src/TeamLoader.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class TeamLoader {
+
+	public static ArrayList<Team> teamLoader(String fileName) throws IOException {
+		ArrayList<Team> teams = new ArrayList<Team>();
+
+		BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+		String st;
+		for(int i=1;(st = br.readLine()) != null;i++)
+			teams.add(new Team(st, i));
+		return teams;
+
+	}
+}
